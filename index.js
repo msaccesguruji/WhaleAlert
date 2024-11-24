5cc68161130c0318d5ab408f582587a9485f67bd
msacces_guruji <159617162+msaccesguruji@users.noreply.github.com>
2024-11-24 20:25:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ app.post('/login', async (req, res) => {
     });
 
     const page = await browser.newPage();
-    await page.goto('https://app.sellerassistant.app/login', { waitUntil: 'domcontentloaded' });
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    );
+    await page.goto('https://app.sellerassistant.app/login', {
+      waitUntil: 'networkidle2',
+      timeout: 60000, // Increase timeout to 60 seconds
+    });
+
 
     // Type the login credentials
     await page.type('#input-1', email);
