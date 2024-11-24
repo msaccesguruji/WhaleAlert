45bbed648b0c628745dd2740b0a0dcfbead0b971
msacces_guruji <159617162+msaccesguruji@users.noreply.github.com>
2024-11-24 19:14:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer@latest');
 const cors = require('cors');
 
 // Initialize Express app
